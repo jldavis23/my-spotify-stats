e7ea44f7f85082bdcab3ecd7e618abaf07bcabb8
Jacey Davis <jaceyd923@gmail.com>
2023-04-15 11:03:31 -0600

    spotify api auth testing

---
+++ src/lib/components/MyComponent.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from 'svelte'
+
+	export let accessToken;
+
+	async function fetchProfile(token) {
+		try {
+			const result = await fetch('https://api.spotify.com/v1/me', {
+				method: 'GET',
+				headers: { Authorization: `Bearer ${token}` }
+			});
+
+			return await result.json();
+		} catch (err) {
+			//console.log(err)
+		}
+	}
+
+    onMount(async () => {
+        const profile = await fetchProfile(accessToken)
+        console.log(profile)
+	});
+
+    
+</script>
+
+<main>
+	<p>hey there</p>
+</main>
