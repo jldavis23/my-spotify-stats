2fa0908b0ae5d1cab897bb794d0ba2230bf5cbc3
Jacey Davis <jaceyd923@gmail.com>
2023-04-18 18:10:08 -0600

    user can view more info about a top track

---
+++ src/lib/components/SongModal.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let selectedTrack;
+</script>
+
+<input type="checkbox" id="my-modal-6" class="modal-toggle" />
+<div class="modal modal-bottom sm:modal-middle">
+	<div class="modal-box">
+		<h3 class="font-bold text-lg">{selectedTrack.name}</h3>
+		<p class="text-sm opacity-50">By {selectedTrack.artists[0].name}</p>
+		<p class="py-4">Album: {selectedTrack.album.name}</p>
+		<p class="py-4">Duration: {selectedTrack.duration_ms}</p>
+		<div class="modal-action">
+			<label for="my-modal-6" class="btn">Yay!</label>
+		</div>
+	</div>
+</div>
--- src/lib/components/TopTracks.svelte
+++ src/lib/components/TopTracks.svelte
@@ -1,6 +1,7 @@
 <script>
 	//Imports
 	import { onMount } from 'svelte';
+	import SongModal from './SongModal.svelte';
 
 	//Props
 	export let accessToken;
@@ -11,28 +12,37 @@
 	let trackUris = [];
 	let btnTimeFrame;
 
+	$:console.log(tracks)
+
 	//Functions
+	onMount(async () => {
+		fetchTopTracks('short_term');
+	});
+
 	async function fetchTopTracks(timeFrame) {
 		if (timeFrame === 'short_term') {
-			btnTimeFrame = 'Last 4 Weeks'
+			btnTimeFrame = 'Last 4 Weeks';
 		} else if (timeFrame === 'medium_term') {
-			btnTimeFrame = 'Last 6 Weeks'
+			btnTimeFrame = 'Last 6 Weeks';
 		} else if (timeFrame === 'long_term') {
-			btnTimeFrame = 'All Time'
+			btnTimeFrame = 'All Time';
 		}
 
 		try {
-			const result = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${timeFrame}`, {
-				method: 'GET',
-				headers: { Authorization: `Bearer ${accessToken}` }
-			});
+			const result = await fetch(
+				`https://api.spotify.com/v1/me/top/tracks?time_range=${timeFrame}`,
+				{
+					method: 'GET',
+					headers: { Authorization: `Bearer ${accessToken}` }
+				}
+			);
 
 			const trackData = await result.json();
-			tracks = trackData.items
-			trackUris = tracks.map(track => track.uri)
-			return tracks
+			tracks = trackData.items;
+			trackUris = tracks.map((track) => track.uri);
+			return tracks;
 		} catch (err) {
-			console.log(err)
+			console.log(err);
 		}
 	}
 
@@ -42,13 +52,13 @@
 				method: 'POST',
 				headers: { Authorization: `Bearer ${accessToken}` },
 				body: JSON.stringify({
-					'name' : `Top Tracks - ${time}`
+					name: `Top Tracks - ${time}`
 				})
 			});
 			const data = await result.json();
-			addSongsToPlaylist(data.id)
+			addSongsToPlaylist(data.id);
 		} catch (err) {
-			console.log(err)
+			console.log(err);
 		}
 	}
 
@@ -57,17 +67,24 @@
 			const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
 				method: 'POST',
 				headers: { Authorization: `Bearer ${accessToken}` },
-				body: JSON.stringify({"uris": trackUris})
-			})
-			console.log('playlist created!')
+				body: JSON.stringify({ uris: trackUris })
+			});
+			console.log('playlist created!');
 		} catch (err) {
-			console.log(err)
+			console.log(err);
 		}
-	}
+	};
 
-	onMount(async () => {
-	    fetchTopTracks('short_term')
-	});
+	const convertToMinSec = (duration) => {
+		let totalMinutes = duration / 1000 / 60;
+		let seconds = Math.round((totalMinutes % Math.floor(totalMinutes)) * 60);
+		let minutes = Math.floor(totalMinutes);
+		if (seconds < 10) {
+			return `${minutes}:0${seconds}`;
+		} else {
+			return `${minutes}:${seconds}`;
+		}
+	};
 
 	// tracks = [
 	// 	{
@@ -76,7 +93,9 @@
 	// 		album: {
 	// 			name: 'PROOF',
 	// 			images: [{ url: 'https://pngimg.com/uploads/square/square_PNG11.png' }]
-	// 		}
+	// 		},
+	// 		duration_ms: 207959,
+	// 		popularity: 69
 	// 	},
 	// 	{
 	// 		name: 'For Youth',
@@ -84,17 +103,31 @@
 	// 		album: {
 	// 			name: 'PROOF',
 	// 			images: [{ url: 'https://pngimg.com/uploads/square/square_PNG11.png' }]
-	// 		}
-	// 	},
+	// 		},
+	// 		duration_ms: 207959,
+	// 		popularity: 69
+	// 	}
 	// ];
 </script>
 
 <div class="md:flex items-center gap-12">
 	<h2 class="text-4xl my-4">Your Top Tracks</h2>
 	<div class="btn-group">
-		<button class="btn btn-xs" class:btn-active={btnTimeFrame === 'Last 4 Weeks'} on:click={() => fetchTopTracks('short_term')}>Last 4 Weeks</button>
-		<button class="btn btn-xs" class:btn-active={btnTimeFrame === 'Last 6 Weeks'} on:click={() => fetchTopTracks('medium_term')}>Last 6 Weeks</button>
-		<button class="btn btn-xs" class:btn-active={btnTimeFrame === 'All Time'} on:click={() => fetchTopTracks('long_term')}>All time</button>
+		<button
+			class="btn btn-xs"
+			class:btn-active={btnTimeFrame === 'Last 4 Weeks'}
+			on:click={() => fetchTopTracks('short_term')}>Last 4 Weeks</button
+		>
+		<button
+			class="btn btn-xs"
+			class:btn-active={btnTimeFrame === 'Last 6 Weeks'}
+			on:click={() => fetchTopTracks('medium_term')}>Last 6 Weeks</button
+		>
+		<button
+			class="btn btn-xs"
+			class:btn-active={btnTimeFrame === 'All Time'}
+			on:click={() => fetchTopTracks('long_term')}>All time</button
+		>
 	</div>
 </div>
 
@@ -104,19 +137,27 @@ sm:grid-flow-col sm:grid-cols-2 sm:grid-rows-[repeat(10,_minmax(0,_1fr))]
 md:grid-cols-3 md:grid-rows-[repeat(7,_minmax(0,_1fr))]"
 >
 	{#each tracks as track, index}
-		<div class="flex items-center space-x-3">
-			<p class="w-1/12">{index + 1}.</p>
-			<div class="avatar">
-				<div class="w-12 h-12">
-					<img src={track.album.images[0].url} alt={track.album.name} />
+		<div class="tooltip tooltip-primary" data-tip="Album: {track.album.name} | Duration: {convertToMinSec(track.duration_ms)}">
+			<div class="flex items-center space-x-3 cursor-default">
+				<p class="w-1/12">{index + 1}.</p>
+
+				<div class="avatar">
+					<div class="w-12 h-12">
+						<img src={track.album.images[0].url} alt={track.album.name} />
+					</div>
 				</div>
-			</div>
-			<div>
-				<div class="font-bold">{track.name}</div>
-				<div class="text-sm opacity-50">{track.artists[0].name}</div>
+
+				<div class="text-left">
+					<div class="font-bold">{track.name}</div>
+					<div class="text-sm opacity-50">{track.artists[0].name}</div>
+				</div>
 			</div>
 		</div>
 	{/each}
 </div>
+
+<!-- {#if selectedTrack}
+	<SongModal {selectedTrack}/>
+{/if} -->
 
 <button class="btn" on:click={() => createPlaylist(btnTimeFrame)}>+ Create Playlist</button>
